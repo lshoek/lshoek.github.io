b2f77d47d9fc8fade8ac780a60c123573b1249e0
lshoek <lesleymartina@hotmail.com>
2020-08-04 18:35:28 +0200

    Fixed scaling issue

--- index.js
+++ index.js
@@ -47,6 +47,6 @@ var canvas = document.getElementById("glslCanvas");
 var sandbox = new GlslCanvas(canvas);
 
 var quality = 2; // 1 = normal
-canvas.width = window.innerWidth / quality;
+canvas.width = window.innerHeight / quality;
 canvas.height = window.innerHeight / quality;
 canvas.style.transform = 'translate(-50%,-50%) scale('+quality+')';
